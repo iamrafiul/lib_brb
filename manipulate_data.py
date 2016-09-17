44036ac5d88f687e8ea381b49531dc9edcaccce1
iamrafiul <iamrafiul@gmail.com>
2016-09-18 00:22:38 +0200

    Added full function create_rule_base method with proper data structure

--- manipulate_data.py
+++ manipulate_data.py
@@ -1,26 +1,6 @@
 import json
-# import ripdb
-
-# json_data = json.dumps({
-#     "x7": {
-#         "antecedent_id": "12345",
-#         "ref_val": [
-#             "800",
-#             "1234"
-#         ]
-#     },
-#     "x8": {
-#         "antecedent_id": "34567",
-#         "ref_val":  [
-#             "1000",
-#             "4567"
-#         ]
-#     }
-# })
-
 
 with open('data.json') as file_data:
-    # import pdb; pdb.set_trace()
     data = json.load(file_data)
 
 
@@ -44,10 +24,21 @@ class Data(object):
 
 class RuleBase(object):
 
-    def __init__(self, obj_list):
-        self.obj_list = obj_list
+    class Rule(object):
+        def __init__(self):
+            self.rule_weight = 1
+            self.antecedent_1 = ""
+            self.antecedent_1_ref_title = ""
+            self.antecedent_2 = ""
+            self.antecedent_2_ref_title = ""
+            self.parent = ""
+            self.consequence_val = list()
+            self.matching_degree = None
+            self.activation_weight = None
+
+    def __init__(self, object_list):
+        self.obj_list = object_list
         self.con_ref_values = list()
-        # self.values = [[None for _ in range(3)] for _ in range(3)]
         self.intermediate_ref_val = 0
 
     def create_rule_base(self):
@@ -55,28 +46,31 @@ class RuleBase(object):
         cons_ref_val_2 = 0
         cons_ref_val_3 = 0
         for each in obj_list:
-            # import pdb; pdb.set_trace()
             if each.name != 'x8':
-                cons_ref_val_1 += float(float(each.attribute_weight) * float(each.ref_val[0]))
-                cons_ref_val_3 += float(float(each.attribute_weight) * float(each.ref_val[2]))
-
+                cons_ref_val_1 += float(
+                    float(each.attribute_weight) * float(each.ref_val[0])
+                )
+                cons_ref_val_3 += float(
+                    float(each.attribute_weight) * float(each.ref_val[2])
+                )
         cons_ref_val_2 += (cons_ref_val_1 + cons_ref_val_3) / 2
 
-        self.con_ref_values.append(cons_ref_val_3)
-        self.con_ref_values.append(cons_ref_val_2)
         self.con_ref_values.append(cons_ref_val_1)
-
-        values = [[0.00 for _ in range(3)] for _ in range(9)]
-
-        for each in values:
-            print each
+        self.con_ref_values.append(cons_ref_val_2)
+        self.con_ref_values.append(cons_ref_val_3)
 
         a = 0
-
         count = 0
+
+        rule_row_list = list()
 
         for i in range(len(obj_list[2].ref_val)):
             for j in range(len(obj_list[1].ref_val)):
+                rules = self.Rule()
+                rules.antecedent_1 = obj_list[2].antecedent_id
+                rules.antecedent_2 = obj_list[1].antecedent_id
+                rules.parent = obj_list[2].parent
+
                 self.intermediate_ref_val = a + float(
                         (float(obj_list[2].attribute_weight) * float(obj_list[2].ref_val[i])) +
                         (float(obj_list[1].attribute_weight) * float(obj_list[1].ref_val[j]))
@@ -84,41 +78,34 @@ class RuleBase(object):
                 is_continue = False
                 for q in range(len(self.con_ref_values)):
                     if self.intermediate_ref_val == self.con_ref_values[q]:
-                        # print "values before inserting 1.00: {}".format(values[i])
-                        values[count][q] = 1.0
+                        rules.antecedent_1_ref_title = obj_list[2].ref_title[i]
+                        rules.antecedent_2_ref_title = obj_list[1].ref_title[j]
+                        rules.consequence_val.append({obj_list[2].ref_title[q]: 1.0})
+                        rule_row_list.append(rules)
+
                         count += 1
-                        print count
-                        # print "Calculated values before continue: {}".format(values[i])
                         is_continue = True
+
                 if is_continue:
                     continue
+                else:
+                    for m in range(0, len(self.con_ref_values)-1):
+                        if (self.con_ref_values[m] > self.intermediate_ref_val) and \
+                                (self.intermediate_ref_val > self.con_ref_values[m+1]):
+                            val_1 = float(
+                                (self.con_ref_values[m] - self.intermediate_ref_val) /
+                                (self.con_ref_values[m] - self.con_ref_values[m+1])
+                            )
+                            val_2 = float(1 - val_1)
 
-                for m in range(0, len(self.con_ref_values)-2):
-                    if (self.con_ref_values[m] > self.intermediate_ref_val) and (self.intermediate_ref_val > self.con_ref_values[m+1]):
-                        print "intermediate_ref_val: {}".format(self.intermediate_ref_val)
+                            rules.antecedent_1_ref_title = obj_list[2].ref_title[i]
+                            rules.antecedent_2_ref_title = obj_list[1].ref_title[j]
+                            rules.consequence_val.append({obj_list[2].ref_title[m+1]: val_1})
+                            rules.consequence_val.append({obj_list[2].ref_title[m]: val_2})
 
-                        val_1 = float((self.con_ref_values[m] - self.intermediate_ref_val) / (self.con_ref_values[m] - self.con_ref_values[m+1]))
-                        values[count][m+1] = val_1
-                        val_2 = float(1 - values[i+j][m+1])
-                        values[count][m] = val_2
-                        # count += 1
-                        # print count
-                print "Calculated values: {}".format(values[count])
-            count += 1
-            print count
-
-        return values
-
-
-
-
-# j = json.loads(json_data)
-
-print "JSON data type: {}".format(type(data))
-
-# for key in data:
-#     for val in data[key]:
-#         print data[key][val]
+                            count += 1
+                rule_row_list.append(rules)
+        return rule_row_list
 
 obj_list = list()
 
@@ -126,18 +113,9 @@ for each in data:
     obj = Data(**data[each])
     obj.name = str(each)
     obj_list.append(obj)
-    # print u.ref_val
-
-
-# import pdb
-#
-# pdb.set_trace()
-
 
 rule_base = RuleBase(obj_list)
 ref_val_list = rule_base.create_rule_base()
 
-print "\n\n\n\n\n"
 for each in ref_val_list:
-    print each
-
+    print "{} {} {} {} {}".format(each.antecedent_1, each.antecedent_1_ref_title, each.antecedent_2, each.antecedent_2_ref_title, each.consequence_val)
