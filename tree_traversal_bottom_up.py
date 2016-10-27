29d7bdb197d9f931a2d0a570e4ae5293d28e57eb
Rafiul Sabbir <mdrhri-6@B0497.macstaff.ltu.se>
2016-10-27 22:57:53 +0200

    Removed 'u' before printing string

--- tree_traversal_bottom_up.py
+++ tree_traversal_bottom_up.py
@@ -13,9 +13,8 @@ for each in data:
     obj.name = str(each)
     obj_list.append(obj)
 
-print "Initial nodes: {}".format([each.antecedent_id for each in obj_list])
 obj_list.sort(key=lambda x: x.is_input == "true", reverse=True)
-print "After sorting: {}".format([each.antecedent_id for each in obj_list])
+print "Initial nodes: {}".format([str(each.antecedent_id) for each in obj_list])
 
 
 
@@ -45,7 +44,7 @@ while(len(obj_list) > 0):
         print "\nAll the current nodes have same parent \"{}\" so the tree traversal is done and the ultimate output is: {}".format(parent, parent)
         break
 
-    print "For {}, parent is: {}".format(obj_list[i].antecedent_id, parent)
+    print "For {}, parent is: {}".format(str(obj_list[i].antecedent_id), parent)
 
     isAllInput = True
     for each in visiting:
@@ -54,12 +53,12 @@ while(len(obj_list) > 0):
 
     if not isAllInput:
         i += 1
-        print "Current Nodes: {}".format([each.antecedent_id for each in visiting])
+        print "Current Nodes: {}".format([str(each.antecedent_id) for each in visiting])
         print "All the children for parent {} is not calculated yet".format(parent)
         obj_list.sort(key=lambda x: x.is_input == "true", reverse=True)
         continue
     else:
-        print "{} children found for parent {}. They are: {}".format(len(visiting), parent, [each.antecedent_id for each in visiting])
+        print "{} children found for parent {}. They are: {}".format(len(visiting), parent, [str(each.antecedent_id) for each in visiting])
         # print "They are: {}".format()
 
         for each in visiting:
@@ -76,9 +75,9 @@ while(len(obj_list) > 0):
                 # current.is_input = 'true'
                 # obj_list.insert(0, current)
                 i = 0
-        print "Remaining nodes for traversal: {}".format([each.antecedent_id for each in obj_list])
+        print "Remaining nodes for traversal: {}".format([str(each.antecedent_id) for each in obj_list])
 
-        print "\nIn iteration {}, {} is calculated and now it's an input node. We've calculated {} subtrees so far.".format(count-1, current.antecedent_id, subtree)
+        print "\nIn iteration {}, {} is calculated and now it's an input node. We've calculated {} subtrees so far.".format(count-1, str(current.antecedent_id), subtree)
         subtree += 1
         obj_list.sort(key=lambda x: x.is_input == "true", reverse=True)
 
